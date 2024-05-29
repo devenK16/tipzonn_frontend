94f34318a40076edf8f2de99c1cd723ae95943b7
devenK16 <devenkhobragade007@gmail.com>
2024-05-29 10:50:34 +0530

    Footer fixed

--- src/components/footer4.js
+++ src/components/footer4.js
@@ -4,74 +4,50 @@ import './footer4.css';
 
 const Footer4 = (props) => {
   return (
-    <div className="footer4-footer7 thq-section-padding">
-      <div className="footer4-max-width thq-section-max-width">
-        <div className="footer4-content">
-          <div className="footer4-logo">
-            <img
-              alt={props.image1Alt}
-              src={props.image1Src}
-              className="footer4-logo1"
-            />
-          </div>
-          <div className="footer4-links">
-            <a href={props.aboutUsLink} className="thq-body-small">{props.link1}</a>
-            <a href={props.contactUsLink} className="thq-body-small">{props.link2}</a>
-            <a href={props.faqsLink} className="thq-body-small">{props.link3}</a>
-            <a href={props.termsOfServiceLink} className="thq-body-small">{props.link4}</a>
-            <a href={props.privacyLink} className="thq-body-small">{props.link5}</a>
-          </div>
-        </div>
-        <div className="footer4-credits">
-          <div className="thq-divider-horizontal"></div>
-          <div className="footer4-row">
-            <div className="footer4-container">
-              <span className="thq-body-small">© 2024 Tipzonn. All right reserved.</span>
+    <div className="footer151-container">
+      <div className="footer151-max-width thq-section-max-width">
+          <div className="footer151-actions">
+            <div className="footer151-container1">
+              <img
+                alt={props.logoAlt}
+                src={props.logoSrc}
+                className="footer151-image1"
+              />
             </div>
-            <div className="footer4-footer-links">
-              <a href={props.privacyLink} className="footer4-text6 thq-body-small">Privacy Policy</a>
+          </div>
+        <div className="footer151-credits">
+          <div className="footer151-row">
+            <div className="footer151-credits1">
+              <a href={props.privacyLink} className="thq-body-small">Privacy Policy</a>
               <a href={props.termsLink} className="thq-body-small">Terms of Use</a>
-              <a href={props.refundLink} className="thq-body-small">Refund/Cancellation</a>
+              <a href={props.cookiesLink} className="thq-body-small">Refunds/Cancellations</a>
             </div>
           </div>
+          <span className="footer151-content3 thq-body-small">
+            {props.copyright}
+          </span>
         </div>
       </div>
     </div>
   );
-};
+}
 
 Footer4.defaultProps = {
-  link1: 'About Us',
-  aboutUsLink: 'https://www.tipzonn.com/about',
-  link2: 'Contact Us',
-  contactUsLink: 'https://www.tipzonn.com/contact',
-  link3: 'FAQs',
-  faqsLink: 'https://www.tipzonn.com/faqs',
-  link4: 'Terms of Service',
-  termsOfServiceLink: 'https://www.tipzonn.com/terms',
-  link5: 'Privacy Policy',
+  cookiesLink: 'https://www.tipzonn.com/refund',
+  copyright: '© 2024 Tipzonn. All rights reserved.',
   privacyLink: 'https://www.tipzonn.com/privacy',
   termsLink: 'https://www.tipzonn.com/terms',
-  refundLink: 'https://www.tipzonn.com/refund',
-  image1Src: 'https://images.unsplash.com/photo-1572016047668-5b5e909e1605?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcxNjgzMzMwMnw&ixlib=rb-4.0.3&q=80&w=1080',
-  image1Alt: 'Tipzonn Logo',
-};
+  logoSrc: '/Tipzonn-logo.png',
+  logoAlt: 'Tipzonn Logo',
+}
 
 Footer4.propTypes = {
-  link1: PropTypes.string,
-  aboutUsLink: PropTypes.string,
-  link2: PropTypes.string,
-  contactUsLink: PropTypes.string,
-  link3: PropTypes.string,
-  faqsLink: PropTypes.string,
-  link4: PropTypes.string,
-  termsOfServiceLink: PropTypes.string,
-  link5: PropTypes.string,
+  cookiesLink: PropTypes.string,
+  copyright: PropTypes.string,
   privacyLink: PropTypes.string,
   termsLink: PropTypes.string,
-  refundLink: PropTypes.string,
-  image1Src: PropTypes.string,
-  image1Alt: PropTypes.string,
-};
+  logoSrc: PropTypes.string,
+  logoAlt: PropTypes.string,
+}
 
 export default Footer4;
