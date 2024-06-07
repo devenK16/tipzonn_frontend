be1937cf653379c8cc28ae0e84615e6b78043964
devenK16 <devenkhobragade007@gmail.com>
2024-06-07 12:48:03 +0530

    Worker Tips ui update 2

--- src/components/WorkerTips.js
+++ src/components/WorkerTips.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import { Meteors } from './ui/meteors';
 import './WorkerTips.css';
 
 function WorkerTips() {
@@ -44,22 +45,25 @@ function WorkerTips() {
           <h1>Welcome,</h1>
           <h2>{worker.name}</h2>
         </div>
-        <div className="total-tips-card">
-          <h2>Total Tips</h2>
-          <p>₹{totalTip.toFixed(2)}</p>
+        <div className="total-tips-card shadow-xl bg-gradient-to-r from-[#5ce4e4] to-[#00bfff] border border-gray-800">
+          <div className="meteor-container">
+            <Meteors number={20} />
+          </div>
+          <h2 className="relative z-10">Total Tips</h2>
+          <p className="relative z-10">₹{totalTip.toFixed(2)}</p>
         </div>
       </div>
       <div className="tips-list">
         <h2>Tips Received :</h2>
-        <div className="tips-container">
+        <div className="tips-container ">
           {tips.map(tip => {
             const date = new Date(tip.date);
             const formattedDate = date.toLocaleDateString();
             const formattedTime = date.toLocaleTimeString();
 
             return (
-              <div key={tip._id} className="tip-item">
-                <div>You have received a tip of ₹{tip.amount.toFixed(2)} on {formattedDate} at {formattedTime}</div>
+              <div key={tip._id} className="tip-item bg-gradient-to-r from-[#5ce4e4] to-[#00bfff]">
+                <div>You have received a tip of ₹<b>{tip.amount.toFixed(2)}</b> on {formattedDate} at {formattedTime}</div>
               </div>
             );
           })}
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,8 @@ module.exports = {
     extend: {
       animation: {
         shimmer: "shimmer 2s linear infinite",
+        
+        "meteor-effect": "meteor 5s linear infinite",
       },
       keyframes: {
         shimmer: {
@@ -21,6 +23,14 @@ module.exports = {
           },
           to: {
             backgroundPosition: "-200% 0",
+          },
+        },
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
           },
         },
       },
